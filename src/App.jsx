0723d74ff6d8677b8948095c3f1bcdba8e9ddbfe
Omkar <omkargodse47@gmail.com>
2025-06-09 16:13:44 +0530

    Zentry SIte Upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Hero from './Components/Hero'
+import About from './Components/About'
+import Navbar from './Components/Navbar'
+import Features from './Components/Features'
+import Story from './Components/Story'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+    
+  )
+}
+
+export default App
